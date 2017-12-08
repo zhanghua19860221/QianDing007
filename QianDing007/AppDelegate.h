a2763cf2d1dc619595c7356144f9f3804c62d97f
张华 <3051942353@qq.com>
2017-12-08 09:07:58 +0800

    Initial Commit

---
+++ QianDing007/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QianDing007
+//
+//  Created by 张华 on 17/12/8.
+//  Copyright © 2017年 张华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QianDing007/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QianDing007
+//
+//  Created by 张华 on 17/12/8.
+//  Copyright © 2017年 张华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
